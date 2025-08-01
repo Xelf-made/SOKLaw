53ae3e30f34468c8afffa2ee437bedfa38259765
Xelf-made <Xelfmad3@gmail.com>
2025-08-01 03:58:43 +0300

    Updated Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -143,7 +143,7 @@ const Footer = () => {
               className="h-8 w-auto"
             />
             <img
-              src="/path/to/lsk-logo.png" // Replace with actual logo URL
+              src="https://soklaw.co.ke/images/law-society-of-kenya.jpg" // Replace with actual logo URL
               alt="Law Society of Kenya"
               className="h-8 w-auto"
             />
